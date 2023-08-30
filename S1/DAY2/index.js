b001a20a5bdc9bcb8591c53f12d4f4d6a04e93b6
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-08-30 12:29:15 +0530

    add

---
+++ S1/DAY2/index.js
@@ -0,0 +1,6 @@
+const [add, sub, multiply, division] = require("./main");
+
+add(10, 10);
+sub(8, 9);
+multiply(5, 8);
+division(4, 2);
---
+++ S1/DAY2/main.js
@@ -0,0 +1,21 @@
+// NOde.js Basics
+
+// Modules
+
+const add = (a, b) => {
+  console.log(a + b);
+};
+
+const sub = (a, b) => {
+  console.log(a - b);
+};
+
+const multiply = (a, b) => {
+  console.log(a * b);
+};
+
+const division = (a, b) => {
+  console.log(a / b);
+};
+
+module.exports = [add, sub, multiply, division];
